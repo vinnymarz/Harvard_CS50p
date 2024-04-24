57b5193834fe379d4a0661f25eb55635f29b0d4b
V Martinez <133036676+vinnymarz@users.noreply.github.com>
2024-04-24 15:28:01 -0400

    Create deep.py

---
+++ Problem Set 1/deep.py
@@ -0,0 +1,6 @@
+convertLife = str(input("What is the Answer to the Great Question of Life, the Universe, and Everything?")).lower().strip()
+
+if convertLife == "42" or convertLife == "forty-two" or convertLife == "forty two":
+    print("Yes")
+else:
+    print("No")
