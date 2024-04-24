d115bb70092ae494ff1b5b0860cfb044423f83d1
V Martinez <133036676+vinnymarz@users.noreply.github.com>
2024-04-24 15:46:30 -0400

    Create figlet.py

---
+++ Problem Set 4/figlet.py
@@ -0,0 +1,24 @@
+import sys
+import random
+from pyfiglet import Figlet
+
+def main():
+    figlet = Figlet()
+    fonts = figlet.getFonts()
+
+    if len(sys.argv) == 1:
+        figlet.setFont(font = random.choice(fonts))
+    elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
+        if sys.argv[2] in fonts:
+            figlet.setFont(font=sys.argv[2])
+        else:
+            sys.exit("Invalid usage")
+    else:
+        sys.exit("Invalid usage")
+
+
+    user_text = input("Input: ").strip()
+    print(figlet.renderText(user_text))
+
+if __name__ == "__main__":
+    main()
