e0a69a7411c1433d299b899435582fbb31351184
V Martinez <133036676+vinnymarz@users.noreply.github.com>
2024-04-26 14:45:46 -0400

    Create numb3rs.py

---
+++ Problem Set 7/numb3rs/numb3rs.py
@@ -0,0 +1,21 @@
+import re
+import sys
+
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+    pattern = r"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+
+    if re.match(pattern, ip):
+        return True
+    else:
+        return False
+
+
+
+
+if __name__ == "__main__":
+    main()
