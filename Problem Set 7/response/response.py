017d9e5391cc089ab908a9568d4b78522ee481fd
V Martinez <133036676+vinnymarz@users.noreply.github.com>
2024-04-26 14:52:54 -0400

    Create response.py

---
+++ Problem Set 7/response/response.py
@@ -0,0 +1,14 @@
+import validators
+
+validators.email('someone@example.com')
+True
+
+def main():
+    user_input = input("What's your email address? ")
+    if validators.email(user_input):
+        print("Valid")
+    else:
+        print("Invalid")
+
+if __name__ == "__main__":
+    main()
